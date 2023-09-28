024d34a5cadfe72fa4e197e50f8cd78e10ff5834
esorochinskiy <evgeny.sorochinskiy@iqr.su>
2023-09-28 15:14:48 +0300

    resources added

---
+++ mainwindow_ru.ts
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="26"/>
+        <location filename="cmake-build-debug/kglife_autogen/include/ui_mainwindow.h" line="141"/>
+        <source>Game Of Life</source>
+        <translation>Игра &quot;Жизнь&quot;</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="82"/>
+        <location filename="mainwindow.ui" line="143"/>
+        <location filename="cmake-build-debug/kglife_autogen/include/ui_mainwindow.h" line="142"/>
+        <location filename="cmake-build-debug/kglife_autogen/include/ui_mainwindow.h" line="145"/>
+        <source>Next Step</source>
+        <translation>Следующий шаг</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="85"/>
+        <location filename="mainwindow.ui" line="105"/>
+        <location filename="cmake-build-debug/kglife_autogen/include/ui_mainwindow.h" line="147"/>
+        <location filename="cmake-build-debug/kglife_autogen/include/ui_mainwindow.h" line="151"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="102"/>
+        <location filename="cmake-build-debug/kglife_autogen/include/ui_mainwindow.h" line="149"/>
+        <location filename="mainwindow.cpp" line="36"/>
+        <source>Start evolution</source>
+        <translation>Начать эволюцию</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="129"/>
+        <location filename="cmake-build-debug/kglife_autogen/include/ui_mainwindow.h" line="152"/>
+        <source>Game</source>
+        <translation>Игра</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="152"/>
+        <location filename="cmake-build-debug/kglife_autogen/include/ui_mainwindow.h" line="143"/>
+        <source>Start/Stop evolution</source>
+        <translation>Начать/Остановить эволюцию</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="36"/>
+        <source>Stop evolution</source>
+        <translation>Остановить эволюцию</translation>
+    </message>
+</context>
+</TS>
